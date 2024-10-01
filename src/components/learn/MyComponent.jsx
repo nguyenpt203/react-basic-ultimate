3c6161d32b0c595a27c5aafc09d783ce43bf22f2
nguyenpt203 <nguyenpt203@gmail.com>
2024-10-01 16:22:01 +0700

    video30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,14 @@
 // () => {}
 // component = html + css + js
+
+import './style.css';
 const MyComponent = () => {
     return (
-        <div>Phung Thai Nguyen</div>
+        <>
+        // fragment
+            <div className="abcs">Phung Thai Nguyen</div>
+            <div className="child" style={{ borderRadius: '20px' }}>child</div>
+        </>
     )
 }
 
