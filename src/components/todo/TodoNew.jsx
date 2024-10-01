112d7445157de9ccb7ee7a7bfe8bb01c588a5975
nguyenpt203 <nguyenpt203@gmail.com>
2024-10-02 00:49:08 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,15 @@
 const TodoNew = (props) => {
     const { addNewTodo } = props;
-
+    const handleClick = () => {
+        alert("Click me")
+    }
+    const handleOnchange = (event) => {
+        console.log(">>> check event", event);
+    }
     return (
         <div className="todo-new">
-            <input className="todo-input" type="text" name="" id="" />
-            <button className="todo-btn" onClick={() => { addNewTodo('Nguyen') }}>Add</button>
+            <input className="todo-input" type="text" name="" id="" onChange={(event) => handleOnchange(event.target.value)} />
+            <button className="todo-btn" onClick={() => { handleClick() }}>Add</button>
         </div>
     )
 }
