3705b9928b2509883f2e8e9b6229c14297d7716b
nguyenpt203 <nguyenpt203@gmail.com>
2024-10-02 15:35:18 +0700

    video 49

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import './header.css';
 const Header = () => {
     return (
         <ul>
-            <li><a className="active" href="/">Home</a></li>
-            <li><a href="/users">User</a></li>
-            <li><a href="/books">Books</a></li>
-        </ul>
+            <li><Link className="active" to={'/'}>Home</Link></li>
+            <li><Link to={'/users'}>User</Link></li>
+            <li><Link to={'/books'}>Books</Link></li>
+        </ul >
     )
 }
 
