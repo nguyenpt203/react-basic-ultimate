66a51dbded0898c92ab32aeda8f5e4418672459b
nguyenpt203 <nguyenpt203@gmail.com>
2024-10-03 09:43:11 +0700

    video 67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,29 +1,40 @@
 import { Button, Input } from "antd";
+import { useState } from "react";
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState("")
+    const [password, setPassWord] = useState("")
+
+    const handleClick = () => {
+        console.log(">>> check state", { fullName, email, phone, password })
+    }
     return (
         <div className="user-form" style={{ margin: '20px 0' }}>
             <div style={{ display: 'flex', gap: '15px', flexDirection: 'column' }}>
                 <div>
                     <span>Full Name</span>
-                    <Input />
+                    <Input value={fullName} onChange={(event) => { setFullName(event.target.value) }} />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input value={email} onChange={(event) => { setEmail(event.target.value) }} />
                 </div>
                 <div>
                     <span>
                         Phone
                     </span>
-                    <Input />
+                    <Input value={phone} onChange={(event) => { setPhone(event.target.value) }} />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password value={password} onChange={(event) => { setPassWord(event.target.value) }} />
                 </div>
                 <div>
-                    <Button type='primary'>Create User</Button>
+                    <Button
+                        onClick={handleClick}
+                        type='primary'>Create User</Button>
                 </div>
             </div>
         </div>
